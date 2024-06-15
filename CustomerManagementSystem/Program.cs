00df86758e36f2e90c31792026a915108a754ee3
Travis-DV <tfindley2@my.stlcc.edu>
2024-06-14 21:21:17 -0500

    Added code

---
+++ CustomerManagementSystem/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Xml.Linq;
+using Validator;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("hello world");
+        CustomerManagementSystem c = new CustomerManagementSystem();
+    }
+}
+
+class Customer
+{
+
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateOnly DateOfJoining { get; set; }
+
+    public Customer(int customerId, string name, string email, DateOnly dateOfJoining)
+    {
+        CustomerId = customerId;
+        Name = name;
+        Email = email;
+        DateOfJoining = dateOfJoining;  
+    }
+
+    public override string ToString()
+    {
+        return $"Customer ID: {CustomerId}, Name: {Name}, Email: {Email}, Joined: {DateOfJoining.ToString("yyyy-MM-dd")}";
+    }
+
+}
+
+class Order
+{
+
+    public int OrderId { get; set; }
+    public int CustomerId { get; set; }
+    public double Amount { get; set; } 
+    public DateOnly OrderDate { get; set; }
+
+    public Order(int orderId, int customrId, double amount, DateOnly orderDate)
+    {
+        OrderId = orderId;
+        CustomerId = customrId;
+        Amount = amount;
+        OrderDate = orderDate;
+    }
+
+    public override string ToString()
+    {
+        return $"Order ID: {OrderId}, Customer ID: {CustomerId}, Amount: ${Amount}, Order Date: {OrderDate.ToString("yyyy-MM-dd")}";
+    }
+
+}
+
+class CustomerManagementSystem
+{
+    
+    private List<Customer> customers = new List<Customer>();
+    private List<Order> orders = new List<Order>();
+
+    public CustomerManagementSystem()
+    {
+        Menu();
+    }
+
+    private void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine(@"1. Add Customer
+2. Add Order
+3. View Customers
+4. View Orders
+5. Calculate Total Sales
+6. Exit");
+
+        string? key = Console.ReadLine();
+        int Choice = -1;
+
+        if (key == null || !int.TryParse(key, out Choice))
+        {
+            Menu();
+        }
+
+        switch (Choice)
+        {
+            case 1:
+                customers.Add(new Customer(this.customers.Count, Validator.Validator.CheckString("Name"), Validator.Validator.GetEmail(), Validator.Validator.GetDate()));
+                Menu();
+                break;
+            case 2:
+                orders.Add(new Order(this.orders.Count, Validator.Validator.CheckInt($"CustomerId (max: {this.customers.Count-1})", this.customers.Count - 1), Validator.Validator.CheckFloat("Price"), Validator.Validator.GetDate()));
+                Menu();
+                break;
+            case 3:
+                this.ListAll("Customer List:", customers);
+                Menu();
+                break;
+            case 4:
+                this.PrintOrders();
+                Menu();
+                break;
+            case 5:
+                this.CalculateTotal();
+                Menu();
+                break;
+            case 6:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    private void ListAll<T>(String Message, List<T> List)
+    {
+        Console.Clear();
+        Console.WriteLine(Message);
+        var query = from item in List
+                    select item;
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
+        Console.ReadLine();
+    }
+
+    private void PrintOrders()
+    {
+        int Choice = Validator.Validator.OneOrTwo("Print All", "Print by Customer Id", "Do you want to");
+        switch (Choice)
+        {
+            case 1:
+                this.ListAll("Order List:", orders);
+                Menu();
+                break;
+            case 2:
+                this.FindOrdersByCustomer(Validator.Validator.CheckInt($"CustomerId (max {this.customers.Count-1})", this.customers.Count-1));
+                Menu();
+                break;
+        }
+    }
+
+    private void FindOrdersByCustomer(int customerId)
+    {
+        var query = orders.Where(order => order.CustomerId == customerId);
+        this.ListAll($"Orders with CustomerId: {customerId}", query.ToList());
+    }
+
+    private void CalculateTotal()
+    {
+
+        var ccustomerSales = orders.GroupBy(order => order.CustomerId)
+            .Select(group => new
+            {
+                CustomerId = group.Key,
+                TotalMoney = group.Sum(order => order.Amount),
+                TotalSales = group.Count()
+            });
+
+        Console.Clear();
+        Console.WriteLine("Total Sales Per Customer: ");
+
+        foreach (var c in ccustomerSales)
+        {
+            Console.WriteLine($"Customer Id: {c.CustomerId}, Total Sales: {c.TotalSales}, Total Dollars: {c.TotalMoney}");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ CustomerManagementSystem/Validator.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    static class Validator
+    {
+        public static int OneOrTwo(string message1, string message2, string message3 = "")
+        {
+            string questionString = $@"1. {message1}
+2. {message2}
+3. Cancel";
+            if (message3 != "")
+            {
+                questionString = $@"{message3}
+1. {message1}
+2. {message2}
+3. Cancel";
+            }
+
+            string? input = null;
+            int output;
+            while (input == null || !int.TryParse(input, out output) || output > 3 || output < 1)
+            {
+                Console.Clear();
+                Console.WriteLine(questionString);
+                input = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static string CheckString(string message)
+        {
+            string? CheckString = null;
+
+            while (CheckString == null || !CheckString.Any(Char.IsLetterOrDigit))
+            {
+                Console.Clear();
+                Console.Write($"{message} (string): ");
+                CheckString = Console.ReadLine();
+            }
+            return CheckString;
+        }
+
+        public static int CheckInt(string message, int Max, int Min = 0)
+        {
+            string? input = null;
+            int output = -1;
+
+            while (input == null || !int.TryParse(input, out output) || output < Min || output > Max)
+            {
+                Console.Clear();
+                Console.Write($"{message} (int): ");
+                input = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static DateOnly GetDate()
+        {
+            int CurrentOrCustom = Validator.OneOrTwo("Set Date Time to Current", "Custom Date Time");
+
+            DateOnly OutTime = DateOnly.FromDateTime(DateTime.Now);
+            switch (CurrentOrCustom)
+            {
+                case 1:
+                    break;
+                case 2:
+                    string? outTime = null;
+
+                    while (outTime == null || !DateOnly.TryParse(outTime, out OutTime))
+                    {
+                        Console.Clear();
+                        Console.Write("Give Date and Time (MM-DD-YYYY HH:MM:SS): ");
+                        outTime = Console.ReadLine();
+                    }
+                    break;
+            }
+
+            return OutTime;
+        }
+
+        public static DateTime GetDateTime()
+        {
+            int CurrentOrCustom = Validator.OneOrTwo("Set Date Time to Current", "Custom Date Time");
+
+            DateTime OutTime = DateTime.Now;
+            switch (CurrentOrCustom)
+            {
+                case 1:
+                    OutTime = DateTime.Now;
+                    break;
+                case 2:
+                    string? outTime = null;
+
+                    while (outTime == null || !DateTime.TryParse(outTime, out OutTime))
+                    {
+                        Console.Clear();
+                        Console.Write("Give Date and Time (MM-DD-YYYY HH:MM:SS): ");
+                        outTime = Console.ReadLine();
+                    }
+                    break;
+            }
+
+            return OutTime;
+        }
+
+        public static double CheckFloat(string message, double Min = 0)
+        {
+            string? input = null;
+            double output = -1;
+
+            while (input == null || !double.TryParse(input, out output) || output < Min)
+            {
+                Console.Clear();
+                Console.Write($"{message} (double): ");
+                input = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static String GetEmail()
+        {
+            string? email = null;
+
+            while (email == null || !email.Contains("@") || !email.Contains("."))
+            {
+                email = Validator.CheckString("Email");
+            }
+
+            return email;
+        }
+    }
+}
